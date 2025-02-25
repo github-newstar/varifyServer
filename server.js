133cbfff657a90609438f4c79c0fc8c023594753
jojo <rss467301127@gmail.com>
2025-02-25 10:54:10 +0000

    server里增加了redis通信和验证码存储

--- server.js
+++ server.js
@@ -1,14 +1,32 @@
 const grpc = require('@grpc/grpc-js')
 const message_proto = require('./proto')
 const const_module = require("./const")
+const redis_module = require("./redis")
 const { v4 : uuidv4 } = require('uuid');
 const emailModule = require("./email")
 async function GetVarifyCode(call, callback) {
     console.log("email is ", call.request.email)
-    try{
-        uniqueId = uuidv4();
+    try {
+        let query_res = await redis_module.GetRedis(const_module.code_prefix + call.request.email);
+        console.log("query_res is ", query_res)
+        let uniqueId = query_res;
+        if (query_res == null) {
+            uniqueId = uuidv4();
+            if (uniqueId.length > 4) {
+                uniqueId = uniqueId.substring(0, 4);
+            }
+            let bres = await redis_module.SetRedisExpire(const_module.code_prefix + call.request.email, uniqueId, 600)
+            if (!bres) {
+                callback(null, {
+                    email: call.request.email,
+                    error: const_module.Errors.RedisErr
+                });
+                return;
+            }
+        }
+
         console.log("uniqueId is ", uniqueId)
-        let text_str =  '您的验证码为'+ uniqueId +'请三分钟内完成注册'
+        let text_str = '您的验证码为' + uniqueId + '请三分钟内完成注册'
         //发送邮件
         let mailOptions = {
             from: '467301127@qq.com',
@@ -18,14 +36,16 @@ async function GetVarifyCode(call, callback) {
         };
         let send_res = await emailModule.SendMail(mailOptions);
         console.log("send res is ", send_res)
-        callback(null, { email:  call.request.email,
-            error:const_module.Errors.Success
-        }); 
-    }catch(error){
+        callback(null, {
+            email: call.request.email,
+            error: const_module.Errors.Success
+        });
+    } catch (error) {
         console.log("catch error is ", error)
-        callback(null, { email:  call.request.email,
-            error:const_module.Errors.Exception
-        }); 
+        callback(null, {
+            email: call.request.email,
+            error: const_module.Errors.Exception
+        });
     }
 }
 function main() {
